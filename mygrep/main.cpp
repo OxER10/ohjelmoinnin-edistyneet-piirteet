34e8ee79ad483021db6346ac37bc3c07031801bf
OxER10 <ozi.jarvinen@gmail.com>
2025-03-03 06:23:18 +0200

    Do 4. increment

--- mygrep/main.cpp
+++ mygrep/main.cpp
@@ -1,9 +1,11 @@
 // Oskari Järvinen
+// 4 Increments, so 5 points
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 // Struct for indexing lines
 struct indexing {
@@ -17,13 +19,16 @@ typedef struct indexing indexingStruct;
 struct options {
     bool lineNo = false;
     bool occurences = false;
+    bool reverseSearch = false;
+    bool caseIgnore = false;
 };
 
-// Call Functions first
-void searchFile(const std::string &file, const std::string sear);
-// Overloading functions
-int searchFile(const std::string &sour, const std::string &sear, const options opt, indexingStruct *index);
+// Call Functions first to get main on top of the stack
 int stringSearch(const std::string &sour, const std::string &sear);
+
+// Overloading functions for different increments
+void searchFile(const std::string &file, const std::string sear);
+int searchFile(const std::string &sour, const std::string &sear, const options opt, indexingStruct *index);
 
 int main(int argc, char *argv[]) {
     try {
@@ -33,10 +38,12 @@ int main(int argc, char *argv[]) {
         if (argc == 1) {
             std::cout << "Give a string from which to search for: ";
             if (!std::getline(std::cin, source) || source.empty()) {
+                // Error if cin is empty
                 throw std::runtime_error("Empty input!");
             }
             std::cout << "Give search string: ";
             if (!std::getline(std::cin, search) || search.empty()) {
+                // Error if cin is empty
                 throw std::runtime_error("Empty input!");
             }
             amount = stringSearch(source, search);
@@ -61,36 +68,51 @@ int main(int argc, char *argv[]) {
             search = argv[2];
             std::string opt = argv[1];
             if (opt.substr(0, 2) != "-o") {
+                // Error if first additional argument was something else than "-o"
                 throw std::runtime_error("Invalid option. Please use \"-o\" for options. ");
             }
             for (char c : opt.substr(2)) {
 				if (c == 'l')  option.lineNo = true;
 				else if (c == 'o') option.occurences = true;
+                else if (c == 'r') option.reverseSearch = true;
+                else if (c == 'i') option.caseIgnore = true;
 				else {
+                    // Error if options afte "-o" are invalid
 					throw std::runtime_error("Invalid options after \"-o\"!\n");
 				}
 			}
             amount = searchFile(source, search, option, indexing_p);
-            for (int i = 0; i < indexing.index.size(); i++) {
-                if (option.lineNo) {
-                    std::cout << indexing.index[i] << ":   ";
+            if (amount > 0) {
+                for (int i = 0; i < indexing.index.size(); i++) {
+                    if (option.lineNo) {
+                        std::cout << indexing.index[i] << ":   ";
+                    }
+                    std::cout << indexing.line[i] << std::endl;
                 }
-                std::cout << indexing.line[i] << std::endl;
+                if (option.occurences) {
+                    if (option.reverseSearch) {
+                        std::cout << "Occurences of lines NOT containing \"" << search << "\" in file: " << amount << std::endl;
+                    }
+                    else {
+                        std::cout << "Occurences of lines containing \"" << search << "\" in file: " << amount << std::endl;
+                    }
+                }
             }
-            if (option.occurences) {
-                std::cout << "Number of strings in file: " << amount << std::endl;
+            else {
+                std::cout << "\"" << search << "\" NOT found in \"" << source << "\"";
             }
-            
         }
         else {
             throw std::runtime_error("Invalid arguments!");
         }
+    // If program encounters known error, outputs error message
     } catch (const std::exception& exc) {
         std::cerr << "Encountered error: " << exc.what() << "\n";
         return 1;
     }
+    // If program encounters unknown error, HOW DID YOU EVEN DO THAT?!
     catch (...) {
-        std::cerr << "Unknown error! Congrats\n";
+        std::cerr << "Unknown error! Congrats!\n";
         return 1;
     }
     return 0;
@@ -103,41 +125,61 @@ void searchFile(const std::string &sour, const std::string sear) {
     int position{0};
     if (txtFile.is_open()){
         while (std::getline(txtFile, line)) {
-            // If string is found, prints it
             position = stringSearch(line, sear);
+            // If string is found, prints it
             if (position != 0) {
                 std::cout << line << std::endl;
             }
         }
-        txtFile.close();
     }
     else {
+        // Error if there is no file with requested name, or you don't have access
         throw std::runtime_error("Couldn't open the file");
     }
+    txtFile.close();
 }
 
-// Second, more complicated iteration of searchFile function. Returns all needed values instead of printing them.
+// Second, more complicated iteration of searchFile function. Returns all needed values instead of printing them for further developement.
 int searchFile(const std::string &sour, const std::string &sear, const options opt, indexingStruct *index) {
     std::ifstream txtFile(sour);
-    std::string line;
+    std::string line, tempLine, tempSear = sear;
     int tot {0}, amount {0}, position{0};
+    // If case ignore is enabled, change all characters on search to lowercase
+    if (opt.caseIgnore) {
+        std::transform(tempSear.begin(), tempSear.end(), tempSear.begin(), ::tolower);
+    }
     if (txtFile.is_open()){
         while (std::getline(txtFile, line)) {
             position++;
-            amount = stringSearch(line, sear);
-            // If string is found, save it to vector via pointer
-            if (amount != 0) {
-                (*index).index.emplace_back(position);
-                (*index).line.emplace_back(line);
-                // Adds string occurences to total values
-                tot += amount;
+            tempLine = line;
+            // If case ignore is enabled, change all characters on line to lowercase
+            if (opt.caseIgnore) {
+                std::transform(tempLine.begin(), tempLine.end(), tempLine.begin(), ::tolower);
+            }
+            amount = stringSearch(tempLine, tempSear);
+            // If reverse search is enabled, saves only lines with no search found
+            if (opt.reverseSearch) {
+                if (amount == 0) {
+                    (*index).index.emplace_back(position);
+                    (*index).line.emplace_back(line);
+                    tot++;
+                }
+            }
+            else {
+                // If string is found, save it to vector via pointer
+                if (amount != 0) {
+                    (*index).index.emplace_back(position);
+                    (*index).line.emplace_back(line);
+                    tot++;
+                }
             }
         }
-        txtFile.close();
     }
     else {
+        // Error if there is no file with requested name, or you don't have access
         throw std::runtime_error("Couldn't open the file");
     }
+    txtFile.close();
     return tot;
 }
 
